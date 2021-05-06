903167d243df4dff20c04c716f71388c4c7aa430
iluzn <65948491+iluzn@users.noreply.github.com>
2021-05-06 19:16:22 +0530

    Create Tea Queue.cpp

---
+++ codeforces/Tea Queue.cpp
@@ -0,0 +1,108 @@
+//      author: iluzn
+//      created: 2021-05-06 19:05:10
+//      problem: B. Tea Queue
+//      URL: https://codeforces.com/problemset/problem/920/B
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <iomanip>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <climits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+using namespace std;
+#define gc getchar_unlocked
+#define fo(i,n) for(i=0;i<n;i++)
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define pb push_back
+#define mp make_pair
+#define nl "\n"
+#define F first
+#define S second
+#define py cout<<"YES"<<nl
+#define pn cout<<"NO"<<nl
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+#define min3(a,b,c)         min(a,min(b,c))
+#define max3(a,b,c)         max(a,max(b,c))
+#define min4(a,b,c,d)       min(a,min(b,min(c,d)))
+#define max4(a,b,c,d)       max(a,max(b,max(c,d)))
+#define max5(a,b,c,d,e)     max(max3(a,b,c),max(d,e))
+#define min5(a,b,c,d,e)     min(min3(a,b,c),min(d,e))
+#define int long long
+#define ll long long
+typedef pair<int, int>  pii;
+typedef vector<int>   vi;
+typedef vector<string>   vs;
+typedef vector<pii>   vpii;
+typedef vector<vi>    vvi;
+const int mod = pow(10,9)+7;
+const int N = 3e5, M = N;
+const int inf =LLONG_MAX;
+const int infn =LLONG_MIN;
+
+// vi G[N];
+// bool visited[N];
+//int level[N];
+
+void iluzn(){
+  int n;
+  cin>>n;
+  vpii a(n);
+  int i;
+  int cur=-1;
+  fo(i,n){
+  	cin>>a[i].F>>a[i].S;
+  	if(i==0){
+  		cout<<a[i].F<<" ";
+  		cur=a[i].F;
+  		cur++;
+  	}
+  	else{
+  		if(cur<=a[i].F){
+  			cout<<a[i].F<<" ";
+  			cur=a[i].F;
+  			cur++;
+  		}
+  		else{
+  			if(cur<=a[i].S){
+  			  cout<<cur<<" ";cur++;}
+  			else
+  			  cout<<0<<" ";
+  			
+  			
+  		}
+  	}
+  }
+  cout<<nl;
+ }
+signed main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t=1;
+    cin>>t;
+     while(t--) iluzn();
+    return 0;
+}
