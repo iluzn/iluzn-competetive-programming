f666794e0d2fe981a61547950c8aeb419fb699c7
iluzn <65948491+iluzn@users.noreply.github.com>
2021-06-11 14:32:28 +0530

    Update and rename insertion only.cpp to singly linked list.cpp

--- data structures/linked list/singly linked list.cpp
+++ data structures/linked list/singly linked list.cpp
@@ -48,7 +48,7 @@ void insert_atpos(int pos,int data){
     }
     cur_pos++;
   }
-  if(pos-cur_pos==0){
+  if(pos-cur_pos==0&&!done){
     done=1;
     temp1->next=node;
   }
@@ -59,6 +59,54 @@ void insert_atpos(int pos,int data){
     printf("ADDED SUCCESSFULLY! \n");
   }
 }
+void delete_begin(){
+  Node *temp=head;
+  temp=head->next;
+  head=temp;
+  printf("DELETED SUCCESSFULLY! \n");
+}
+void delete_end(){
+  Node* temp=head;
+  Node* temp1;
+  while(temp->next!=NULL){
+    temp1=temp;
+    temp=temp->next;
+  }
+  temp1->next=NULL;
+  printf("DELETED SUCCESSFULLY! \n");
+}
+void delete_atpos(int pos){
+  bool done=0;
+  if(pos==1){
+    delete_begin();
+    return;
+  }
+  Node* temp=head;
+  Node *temp1;
+  int cur_pos=1;
+  while(temp->next!=NULL){
+    if(cur_pos==pos){
+       temp1->next=temp->next;
+       done=1;
+       break;
+    }
+    else{
+      temp1=temp;
+      temp=temp->next;
+    }
+    cur_pos++;
+  }
+  if(pos==cur_pos&&!done){
+    temp1->next=NULL;
+    done=1;
+  }
+  if(done){
+    printf("DELETED SUCCESSFULLY! \n");
+  }
+  else{
+     printf("INVALID INPUT \n");
+  }
+}
 void print(){
   Node *temp=head;
   while(temp!=NULL){
@@ -66,17 +114,35 @@ void print(){
       printf("%d",temp->data);
     else
     printf("%d->",temp->data);
-
+  
     temp=temp->next;
   }
+  printf("\n");
 
+}
+void reverse(){
+  Node *prev=NULL;
+  Node *temp=head;
+  Node *nex=NULL;
+  while(temp!=NULL){
+     nex=temp->next;
+     temp->next=prev;
+     prev=temp;
+     temp=nex;
+  }
+  head=prev;
 }
 int main(){
   insert_begin(5);
+  insert_begin(62);
   insert_end(6);
   insert_end(63);
   insert_atpos(4,234);
-
+  delete_begin();
+  delete_end();
+  print();
+  delete_atpos(2);
+  reverse();
   print();
   return 0;
 }
